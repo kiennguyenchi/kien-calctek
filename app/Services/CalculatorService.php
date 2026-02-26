fe8622da73408a073f6e0fec1bfa9c798296f61b
Kien Nguyen <kiennguyenchi11@gmail.com>
2026-02-26 17:38:17 -0500

    using math parser package to ensure security

--- app/Services/CalculatorService.php
+++ app/Services/CalculatorService.php
@@ -3,14 +3,12 @@
 namespace App\Services;
 
 use Exception;
+use NXP\MathExecutor;
 
 class CalculatorService
 {
     public function calculate(string $expression): float
     {
-        // Basic validation to prevent code injection and ensure only valid characters are processed
-        // This is a temporary fix to use eval() for math execution
-        // Better solutions would be to use a proper math expression parser library, or write our own parser
         if (!preg_match('/^([0-9\+\-\*\/\(\)\.\^\s]|sqrt)+$/i', $expression)) {
             throw new Exception("Invalid or unsupported mathematical characters detected.");
         }
@@ -18,13 +16,10 @@ class CalculatorService
         //Removing whitespace
         $expression = str_replace(' ', '', $expression);
 
-        //PHP treats ^ as bitwise logic, so we must convert it to ** for math powers
-        $expression = str_replace('^', '**', $expression);
-
         try {
-            $result =  eval('return ' . $expression . ';');
-            return $result;
-        } catch (\Throwable $e) {
+            $executor = new MathExecutor();
+            return $executor->execute($expression);
+        } catch (Exception $e) {
             throw new Exception("Invalid characters entered");
         }
     }
